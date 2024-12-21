be9aefe04298bcb9b63e54182fe68faabfc20416
marcocondrache <marcomihaicondrache@gmail.com>
2024-12-21 12:50:08 +0100

    feat: debug events

--- src/hooks/use-long-press.ts
+++ src/hooks/use-long-press.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@/components/ui/use-toast";
 import type { RefCallback } from "react";
 
 type StartEvent = "pointerdown" | "mousedown" | "touchstart";
@@ -14,15 +15,45 @@ export function useLongPress<T extends Element>({
   onEnd,
   events: [startEvent, endEvent] = ["pointerdown", "pointerup"],
 }: LongPressOptions) {
+  const { toast } = useToast();
+
+  const debugEvents = [
+    "pointerdown",
+    "pointerup",
+    "pointercancel",
+    "mousedown",
+    "mouseup",
+    "mousecancel",
+    "touchstart",
+    "touchend",
+    "touchcancel",
+    "contextmenu",
+  ];
+
+  const debugEvent = (event: Event) => {
+    toast({
+      title: event.type,
+      description: event.type,
+    });
+  };
+
   const ref = (element: T | null) => {
     if (!element) return;
 
     element.addEventListener(startEvent, onStart);
     element.addEventListener(endEvent, onEnd);
 
+    for (const event of debugEvents) {
+      element.addEventListener(event, debugEvent);
+    }
+
     return () => {
       element.removeEventListener(startEvent, onStart);
       element.removeEventListener(endEvent, onEnd);
+
+      for (const event of debugEvents) {
+        element.removeEventListener(event, debugEvent);
+      }
     };
   };
 
