fc906b86708f20bbaea30a9c912ae472289f58a6
marcocondrache <marcomihaicondrache@gmail.com>
2024-12-23 11:07:39 +0100

    feat: add some deps

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -3,4 +3,10 @@
 # Exit on error
 set -e
 
+apt install -y libpam-systemd
+apt install -y seatd
+apt install -y xdg-user-dirs
+apt install -y wayfire
+apt install -y libgl1-mesa-dri
+
 echo "Provisioning..."
