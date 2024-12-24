ec59132b648ad647f6ab7b1a2f98bfca84c4a172
marcocondrache <marcomihaicondrache@gmail.com>
2024-12-24 17:25:00 +0100

    fix: disable cursor on hybrid devices

--- src/hooks/use-disable-defaults.ts
+++ src/hooks/use-disable-defaults.ts
@@ -1,7 +1,12 @@
 import { isServer, isTauri } from "@/lib/constants";
+import { deviceType } from "detect-it";
 import { useState } from "react";
 
 function disableDefaults() {
+  // Temporary fix until raspberry pi publishes package labwc 0.8.2
+  if (deviceType === "hybrid" || deviceType === "touchOnly")
+    document.body.style.cursor = "none";
+
   document.addEventListener(
     "contextmenu",
     (event) => {
