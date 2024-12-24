b08684e6bf76829ceb59a158e7f4651ecd667f90
marcocondrache <marcomihaicondrache@gmail.com>
2024-12-24 15:07:18 +0100

    refactor: switch to labwc

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -3,12 +3,6 @@
 # Exit on error
 set -e
 
-apt install -y dbus-user-session
-apt install -y libpam-systemd
-apt install -y seatd
-apt install -y xdg-user-dirs
-apt install -y wayfire
-apt install -y libgl1-mesa-dri
 apt install -y libwebkit2gtk-4.1-0
 
 echo "Provisioning..."
