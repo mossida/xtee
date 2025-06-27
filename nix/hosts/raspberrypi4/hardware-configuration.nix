34234846c8995981805ec44bddc7c45c437fa60a
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-06-27 17:08:45 +0200

    chore: add shutdown dts
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- nix/hosts/raspberrypi4/hardware-configuration.nix
+++ nix/hosts/raspberrypi4/hardware-configuration.nix
@@ -1,4 +1,67 @@
 {
+  boot = {
+    kernelParams = [ "vt.global_cursor_default=0" ];
+  };
+
+  hardware.raspberry-pi."4" = {
+    gpio.enable = true;
+    fkms-3d.enable = true;
+  };
+
+  hardware.deviceTree.overlays = [
+    {
+      name = "gpio-shutdown";
+      dtsText = ''
+        /dts-v1/;
+        /plugin/;
+        / {
+        	compatible = "brcm,bcm2711";
+        	fragment@0 {
+        		target = <&gpio>;
+        		__overlay__ {
+        			pin_state: shutdown_button_pins {
+        				brcm,pins = <3>;
+        				brcm,function = <0>;
+        				brcm,pull = <2>;
+        			};
+        		};
+        	};
+        	fragment@1 {
+        		target-path = "/soc";
+        		__overlay__ {
+        			shutdown_button {
+        				compatible = "gpio-keys";
+
+        				pinctrl-names = "default";
+        				pinctrl-0 = <&pin_state>;
+
+        				status = "okay";
+
+        				button: shutdown {
+        					label = "shutdown";
+        					linux,code = <116>;
+        					gpios = <&gpio 3 1>;
+        					debounce-interval = <100>;
+        				};
+        			};
+        		};
+        	};
+
+        	__overrides__ {
+        		gpio_pin = <&button>,"gpios:4",
+        		           <&pin_state>,"brcm,pins:0";
+
+        		gpio_pull = <&pin_state>,"brcm,pull:0";
+
+        		active_low = <&button>,"gpios:8";
+        		debounce = <&button>,"debounce-interval:0";
+        	};
+
+        };
+      '';
+    }
+  ];
+
   fileSystems = {
     "/boot" = {
       device = "/dev/disk/by-label/BOOT";
