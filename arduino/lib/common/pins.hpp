23e854c880ea3fbd877f4d1888e20949a5a5ca06
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-07-07 15:18:11 +0200

    chore: updates
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- arduino/lib/common/pins.hpp
+++ arduino/lib/common/pins.hpp
@@ -15,8 +15,8 @@ namespace pins
     static const uint8_t ACTUATOR_DIR = 8;
     static const uint8_t ACTUATOR_PWM = 7;
 
-    static const Motor MOTOR_1 = {9, 12, 13};
-    static const Motor MOTOR_2 = {10, 7, 6};
+    static const Motor MOTOR_1 = {3, 2, 4};
+    static const Motor MOTOR_2 = {6, 5, 7};
 
     static const Motor *MOTORS[] = {&MOTOR_1, &MOTOR_2};
     static const size_t MOTORS_COUNT = 2;
--- nix/hosts/raspberrypi4/hardware-configuration.nix
+++ nix/hosts/raspberrypi4/hardware-configuration.nix
@@ -62,17 +62,5 @@
     }
   ];
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/BOOT";
-      fsType = "vfat";
-    };
-
-    "/firmware" = {
-      device = "/dev/disk/by-label/FIRMWARE";
-      fsType = "vfat";
-    };
-  };
-
   nixpkgs.hostPlatform.system = "aarch64-linux";
 }
