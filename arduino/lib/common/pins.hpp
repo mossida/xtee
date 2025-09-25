3510fccdd1134739b6ecfe3319aad4ca4197d915
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-09-25 19:56:25 +0200

    chore: pins
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- arduino/lib/common/pins.hpp
+++ arduino/lib/common/pins.hpp
@@ -7,18 +7,19 @@ namespace pins
 {
     struct Motor
     {
-        const uint8_t step;
-        const uint8_t dir;
-        const uint8_t enable;
+        const uint8_t step; // Use gpio pins
+        const uint8_t dir; // Use D pins
+        const uint8_t enable; // Use D pins
     };
 
     static const uint8_t ACTUATOR_DIR = 3;
     static const uint8_t ACTUATOR_PWM = 2;
 
-    // static const Motor MOTOR_1 = {6, 5, 7};
-    // static const Motor MOTOR_2 = {9, 8, 10};
-    static const Motor MOTOR_1 = {3, 2, 4};
-    static const Motor MOTOR_2 = {6, 5, 7};
+
+    static const Motor MOTOR_1 = {6, 2, 4};
+    static const Motor MOTOR_2 = {9, 5, 7};
+    // static const Motor MOTOR_1 = {9, 2, 4};
+    // static const Motor MOTOR_2 = {6, 5, 7};
 
     static const Motor *MOTORS[] = {&MOTOR_1, &MOTOR_2};
     static const size_t MOTORS_COUNT = 2;
